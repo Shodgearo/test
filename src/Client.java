4352b61f69dd0cf7b5c7ecd2e969808b10339b20
Shodgearo <shodkvest@gmail.ru>
2018-07-09 17:57:19 +0600

    Tools light client settings

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+// Клиент
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("localhost", 8189)) {
+            Scanner in = new Scanner(socket.getInputStream());
+            PrintWriter out = new PrintWriter(socket.getOutputStream());
+
+            Scanner scanner = new Scanner(System.in);
+
+            while (true) {
+                System.out.println(in.nextLine());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
