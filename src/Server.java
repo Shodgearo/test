5e8dc88b964a46b0abf101f85d3cfba8cb785e4b
Shodgearo <shodkvest@gmail.ru>
2018-07-09 17:57:37 +0600

    Tools light server settings

--- src/Server.java
+++ src/Server.java
@@ -6,20 +6,26 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class Main {
+public class Server {
     public static void main(String[] args) {
-        try (ServerSocket server = new ServerSocket(80);
+        try (ServerSocket server = new ServerSocket(8189);
              Socket socket = server.accept()) {
+
             Scanner in = new Scanner(socket.getInputStream());
-            PrintWriter out = new PrintWriter(socket.getOutputStream());
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
             out.println("Подключение создано..." + socket.getInetAddress());
+
             while (true) {
-                
+                Thread.sleep(1000);
+
+                System.out.println("Horowo sliwno?.");
+                out.println("Yes, normalno.");
             }
         } catch (IOException e) {
-            System.out.println("Ошибка соединения.");
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            System.out.println("Ошибка при выполнении потока.");
         }
-
     }
 }
