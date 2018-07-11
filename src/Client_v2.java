1ab98f1ecdcf9a53ef3a497e29d6d62a1d10060c
Shodgearo <shodkvest@gmail.ru>
2018-07-11 17:30:59 +0600

    In progress

--- src/Client_v2.java
+++ src/Client_v2.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
 
 public class Client_v2 {
+    public static void main(String[] args) {
+        String url = "https://www.youtube.com";
+
+        try {
+            URLConnection connection = new URL(url).openConnection();
+            Scanner scanner = new Scanner(connection.getInputStream());
+
+            while (scanner.hasNext()) {
+                System.out.println(scanner.next());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/Server_v2.java
+++ src/Server_v2.java
@@ -1,4 +1,5 @@
-package PACKAGE_NAME;
-
 public class Server_v2 {
+    public static void main(String[] args) {
+
+    }
 }
