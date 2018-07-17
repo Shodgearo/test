5438e3df0ef9315862b5933382b7bb2c0bd2d0f1
Shodgearo <shodkvest@gmail.ru>
2018-07-17 10:42:15 +0600

    Simple example server

--- src/Server_v2.java
+++ src/Server_v2.java
@@ -1,5 +1,20 @@
+//
+
+import com.sun.net.httpserver.HttpsServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class Server_v2 {
     public static void main(String[] args) {
+        try {
+            HttpsServer httpsServer = HttpsServer.create(new InetSocketAddress("localhost", 8189), 0);
 
+            while (true) {
+                System.out.println("Hi");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
