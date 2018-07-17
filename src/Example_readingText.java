a40eba1c9a98605f325088195eda4e699841ce2d
Shodgearo <shodkvest@gmail.ru>
2018-07-17 14:55:20 +0600

    Read in file

--- src/Example_readingText.java
+++ src/Example_readingText.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Example_readingText {
+    public static void main(String[] args) {
+        File file = new File("/home/meritokrat/Рабочий стол/1.html");
+
+        try {
+            Scanner br = new Scanner(new FileInputStream(file));
+
+            while (br.hasNextLine()) {
+                System.out.println(br.next());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
