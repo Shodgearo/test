ed8192f20137f615a9d34899b975251d4a6b8822
Shodgearo <shodkvest@gmail.ru>
2018-07-20 16:05:07 +0600

    Commenting for test

--- src/Server_v2_1.java
+++ src/Server_v2_1.java
@@ -1,23 +1,23 @@
-import java.io.*;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class Server_v2_1 {
     public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(8189);
              Socket socket = server.accept()) {
             OutputStream out = socket.getOutputStream();
-            File file = new File("/home/meritokrat/Рабочий стол/2.html");
+//            File file = new File("/home/meritokrat/Рабочий стол/2.html");
 
-            Scanner scan = new Scanner(new FileInputStream(file));
-            StringBuilder stringBuilder = new StringBuilder();
-            while (scan.hasNext()) {
-                stringBuilder.append(scan.next());
-            }
-
-            String s = stringBuilder.toString();
-//            String s = "<html><body><h1>Hello</h1></body></html>";
+//            Scanner scan = new Scanner(new FileInputStream(file));
+//            StringBuilder stringBuilder = new StringBuilder();
+//            while (scan.hasNext()) {
+//                stringBuilder.append(scan.next());
+//            }
+//
+//            String s = stringBuilder.toString();
+            String s = "<html><body><h1>Hello</h1></body></html>";
 //            System.out.println(s);
             String response = "HTTP/1.1 200 OK\r\n" +
                     "Content-Type: text/html\r\n" +
